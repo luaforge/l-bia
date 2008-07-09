785f052f45083fda92b1164624f154e27f417ece
Breno Ramalho Lemes <br_lemes@luaforge.net>
2008-07-09 20:32:39 +0000

    * lua_allc based for smaller size. * new id implementation (for 0.2 series).

--- l-bia/src/l-bia.c
+++ l-bia/src/l-bia.c
@@ -1,5 +1,5 @@
 /*
- *  "$Id: l-bia.c,v 1.2 2008-06-26 23:38:16 br_lemes Exp $"
+ *  "$Id: l-bia.c,v 1.3 2008-07-09 20:32:39 br_lemes Exp $"
  *  Lua Built-In program (L-Bia)
  *  A self-running Lua interpreter. It turns your Lua program with all
  *  required modules and an interpreter into a single stand-alone program.
@@ -13,28 +13,18 @@
  *  http://l-bia.luaforge.net/
  */
 
-#ifdef _WIN32
-#include <windows.h>
-#endif
+#ifndef L_BIA_C
+#define L_BIA_C
 
 #include "lbconf.h"
-
-#define LB_FLATMD 101
-#define LB_MLZOMD 102
-#define LB_FLATID "Flat"
-#define LB_MLZOID "mLZO"
 
 lua_State *L = NULL;
 
 typedef struct {
-  char open;     /* <                   */
-  char sign[6];  /* L-Bia#              */
-  char mode[4];  /* Flat|mLZO           */
-  char sep1;     /* :                   */
-  char nlen[10]; /* normal lenght       */
-  char sep2;     /* :                   */
-  char dlen[10]; /* decompressed lenght */
-  char close;    /* >                   */
+  char id[4];       /* LB02 */
+  uint32_t dlen;    /* decompressed lenght */
+  uint32_t adler32; /* adler32 checksum */
+  uint32_t nlen;    /* compressed or normal lenght */
 } lb_idtype;
 
 void lb_error(const char *msg) {
@@ -43,51 +33,17 @@ void lb_error(const char *msg) {
   exit(EXIT_FAILURE);
 }
 
-void lb_cannot(const char *what,const char *name) {
+void lb_cannot(const char *what, const char *name) {
   lua_pushfstring(L,"Cannot %s %s: %s",what,name,strerror(errno));
   lb_error(lua_tostring(L,-1));
 }
 
-static const luaL_Reg luserlibs[] = {
-#ifdef LBCONF_LIB_BASE
-  {"",luaopen_base},
-#endif
-#ifdef LBCONF_LIB_LOAD
-  {LUA_LOADLIBNAME,luaopen_package},
-#endif
-#ifdef LBCONF_LIB_TABLE
-  {LUA_TABLIBNAME,luaopen_table},
-#endif
-#ifdef LBCONF_LIB_IO
-  {LUA_IOLIBNAME,luaopen_io},
-#endif
-#ifdef LBCONF_LIB_OS
-  {LUA_OSLIBNAME,luaopen_os},
-#endif
-#ifdef LBCONF_LIB_STRING
-  {LUA_STRLIBNAME,luaopen_string},
-#endif
-#ifdef LBCONF_LIB_MATH
-  {LUA_MATHLIBNAME,luaopen_math},
-#endif
-#ifdef LBCONF_LIB_DB
-  {LUA_DBLIBNAME,luaopen_debug},
-#endif
-  {NULL,NULL}
-};
-
-LUALIB_API void luaL_openuserlibs(lua_State *L) {
-  const luaL_Reg *lib = luserlibs;
-  for (;lib->func;lib++) {
-    lua_pushcfunction(L,lib->func);
-    lua_pushstring(L,lib->name);
-    lua_call(L,1,0);
-  }
-}
-
-int main(int argc, char *argv[]){
-  int lb_nlen, lb_dlen, lb_mode;
+int main(int argc, char *argv[]) {
   lb_idtype lb_id;
+  lzo_uint  lb_overhead;
+  lzo_uint  lb_offset;
+  lzo_bytep lb_data;
+  lzo_bytep lb_temp;
 
 #ifdef _WIN32
   char selfname[MAX_PATH];
@@ -102,12 +58,11 @@ int main(int argc, char *argv[]){
   L = luaL_newstate();
   if (L == NULL) lb_error("Not enough memory");
 
-/*luaL_openlibs(L); */
-  luaL_openuserlibs(L);
+  luaL_openlibs(L);
 
-  LBCONF_USERFUNC_INIT(L);
+//  LBCONF_USERFUNC_INIT(L);
 
-  /* extracting pass */ 
+  /* open and load */
   FILE *lb_file = fopen(argv[0],"rb");
   if (lb_file == NULL)
     lb_cannot("open",argv[0]);
@@ -115,69 +70,51 @@ int main(int argc, char *argv[]){
     lb_cannot("seek",argv[0]);
   if (fread(&lb_id,sizeof(lb_id),1,lb_file)!=1)
     lb_cannot("read",argv[0]);
-
-  /* mode detect subpass */
-  if (memcmp(lb_id.mode,LB_FLATID,4)==0)
-    lb_mode = LB_FLATMD;
-  else if (memcmp(lb_id.mode,LB_MLZOID,4)==0)
-    lb_mode = LB_MLZOMD;
-  else
-    lb_error("Missing, corrupted or incompatible script overlay");
-
-  /* getting mode parameters subpass */
-  lua_pushlstring(L,lb_id.nlen,10);
-  lb_nlen = lua_tointeger(L,-1);
-  lua_pop(L,1);
-  if (lb_nlen == 0)
-    lb_error("Missing, corrupted or incompatible script overlay");
-  if (lb_mode == LB_MLZOMD) {
-    lua_pushlstring(L,lb_id.dlen,10);
-    lb_dlen = lua_tointeger(L,-1);
-    lua_pop(L,1);
-  }
-
-  /* data loading subpass */
-  if (fseek(lb_file,-(sizeof(lb_id)+lb_nlen),SEEK_END)!=0)
+  if (memcmp(lb_id.id,"LB02",4)!=0)
+    lb_error("Missing overlay");
+  if (fseek(lb_file,-(sizeof(lb_id)+lb_id.nlen),SEEK_END)!=0)
     lb_cannot("seek",argv[0]);
-  char *lb_data = (char*)alloca(lb_nlen);
-  if (lb_data == NULL) lb_error("Not enough memory");
-  if (fread(lb_data,lb_nlen,1,lb_file)!=1)
+  if (lb_id.dlen != 0) {
+    lb_overhead = lb_id.dlen / 16 + 64 + 3;
+    lb_offset = lb_id.dlen + lb_overhead - lb_id.nlen;
+    lb_temp = (lzo_bytep)alloca(lb_id.dlen + lb_overhead);
+    lb_data = lb_temp + lb_offset;
+  } else
+    lb_data = (lzo_bytep)alloca(lb_id.nlen);
+  if (fread(lb_data,lb_id.nlen,1,lb_file)!=1)
     lb_cannot("read",argv[0]);
   fclose(lb_file);
 
-  /* decompress pass */
-  if (lb_mode == LB_MLZOMD) {
-    int r;
+  /* checksum */
+  if (lzo_adler32(0,(lzo_bytep)lb_data,lb_id.nlen)!=lb_id.adler32)
+    lb_error("Bad checksum");
+
+  /* decompress */
+  if (lb_id.dlen != 0) {
     lzo_uint new_len;
-    lzo_bytep out = (lzo_bytep)alloca(lb_dlen);
-    if (out == NULL) lb_error("Not enough memory");
-    r = lzo1x_decompress(lb_data,lb_nlen,out,&new_len,NULL);
-    if (r == LZO_E_OK || lb_dlen == new_len)
-      lua_pushlstring(L,(const char*)out,lb_dlen);
-    else
-      lb_error("decompress failed");
-  } else lua_pushlstring(L,lb_data,lb_nlen);
+    int r = lzo1x_decompress(lb_data,lb_id.nlen,lb_temp,&new_len,NULL);
+    if (r != LZO_E_OK || new_len != lb_id.dlen)
+      lb_error("Overlapping decompression failed");
+    lb_data = lb_temp;
+  } else 
+    lb_id.dlen = lb_id.nlen;
 
-  lua_newtable(L);
-  {
-    int i = 0;
-    for (; i <= argc; i++) {
-      lua_pushstring(L,argv[i]);
-      lua_rawseti(L,-2,i);
-    }
-  }
-  lua_setglobal(L,"arg");
+  char *buf = lb_data;
+  uint32_t ptr = 0;
+  do {
+    char  _type = *buf++;
+    char  _nlen = *buf++;
+    char *_name = alloca((size_t)_nlen+1);
+    memcpy(_name,buf,_nlen); _name[_nlen] = '\0'; buf += _nlen;
+    uint32_t _size; memcpy(&_size,buf,4); buf += 4;
+    buf += _size;
+    ptr += 2 + _nlen + 4 + _size;
+    printf("type: %d\nnlen: %d\nname: %s\n",_type,_nlen,_name);
+  } while (ptr < lb_id.dlen);
 
-  /* runtime pass */
-  if (luaL_loadbuffer(L,lua_tostring(L,-1),lua_objlen(L,-1),argv[0]))
-    lb_error(lua_tostring(L,-1));
-  int i = 1;
-  for (; argv[i]; i++)
-    lua_pushstring(L,argv[i]);
-  if (lua_pcall(L,i-1,0,0))
-    lb_error(lua_tostring(L,-1));
-  lua_pop(L,1);
-
-  LBCONF_USERFUNC_DONE(L);
+//  LBCONF_USERFUNC_DONE(L);
   lua_close(L);
+  return 0;
 }
+
+#endif
